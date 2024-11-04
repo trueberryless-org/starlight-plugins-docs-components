b0d7b95b8cca9e38b1fcfde5ba56359e3953a02e
trueberryless <trueberryless+github@gmail.com>
2024-11-04 04:08:47 +0100

    fix humanizeList and docs mistake
    deploy

---
+++ packages/starlight-plugins-docs-components/libs/humanizeList.ts
@@ -0,0 +1,38 @@
+export default function humanizeList(
+    list: any[],
+    options?: { conjunction?: any; skipConjunction?: any; oxfordComma?: any }
+) {
+    if (!Array.isArray(list)) {
+        throw new TypeError("humanize-list expected an array");
+    }
+
+    options = options || {};
+    options.conjunction = options.conjunction || "and";
+
+    var listLength = list.length;
+
+    if (listLength === 1) {
+        return list[0];
+    }
+
+    if (options.skipConjunction) {
+        return list.join(", ");
+    }
+
+    var humanizedList = "";
+    for (var i = 0; i < listLength; i++) {
+        if (i === listLength - 1) {
+            if (options.oxfordComma) {
+                humanizedList += ",";
+            }
+
+            humanizedList += " " + options.conjunction + " ";
+        } else if (i !== 0) {
+            humanizedList += ", ";
+        }
+
+        humanizedList += list[i];
+    }
+
+    return humanizedList;
+}
