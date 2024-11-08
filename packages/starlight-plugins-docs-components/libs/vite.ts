72eb7b061f73ae7add002338838a5662ad003690
trueberryless <trueberryless+github@gmail.com>
2024-11-08 09:02:48 +0100

    update many files

--- packages/starlight-plugins-docs-components/libs/vite.ts
+++ packages/starlight-plugins-docs-components/libs/vite.ts
@@ -3,67 +3,69 @@ import type { ImageMetadata, ViteUserConfig } from "astro";
 import type { StarlightPluginsDocsComponentsConfig } from "..";
 
 export function vitePluginStarlightPluginsDocsComponentsConfig(
-    config: StarlightPluginsDocsComponentsConfig
+  config: StarlightPluginsDocsComponentsConfig,
 ): VitePlugin {
-    const modules = {
-        "virtual:starlight-plugins-docs-components-context": `export default {
+  const modules = {
+    "virtual:starlight-plugins-docs-components-context": `export default {
             githubOwner: ${JSON.stringify(config.githubOwner)},
             pluginName: ${JSON.stringify(config.pluginName)},
             showcaseFilepath: ${JSON.stringify(config.showcaseFilepath)},
             showcaseProps: {
                 entries: [
                     ${config.showcaseProps.entries
-                        .map(
-                            (entry) => `{
+                      .map(
+                        (entry) => `{
                                 thumbnail: await import(${JSON.stringify(entry.thumbnail)}),
                                 href: ${JSON.stringify(entry.href)},
                                 title: ${JSON.stringify(entry.title)},
                                 description: ${JSON.stringify(entry.description)}
-                            }`
-                        )
-                        .join(",")}
+                            }`,
+                      )
+                      .join(",")}
                 ]
             }
         }`,
-    };
+  };
 
-    const moduleResolutionMap = Object.fromEntries(
-        (Object.keys(modules) as (keyof typeof modules)[]).map((key) => [
-            resolveVirtualModuleId(key),
-            key,
-        ])
-    );
+  const moduleResolutionMap = Object.fromEntries(
+    (Object.keys(modules) as (keyof typeof modules)[]).map((key) => [
+      resolveVirtualModuleId(key),
+      key,
+    ]),
+  );
 
-    return {
-        name: "vite-plugin-starlight-plugins-docs-components",
-        load(id) {
-            const moduleId = moduleResolutionMap[id];
-            return moduleId ? modules[moduleId] : undefined;
-        },
-        resolveId(id) {
-            return id in modules ? resolveVirtualModuleId(id) : undefined;
-        },
-    };
+  return {
+    name: "vite-plugin-starlight-plugins-docs-components",
+    load(id) {
+      const moduleId = moduleResolutionMap[id];
+      return moduleId ? modules[moduleId] : undefined;
+    },
+    resolveId(id) {
+      return id in modules ? resolveVirtualModuleId(id) : undefined;
+    },
+  };
 }
 
-function resolveVirtualModuleId<TModuleId extends string>(id: TModuleId): `\0${TModuleId}` {
-    return `\0${id}`;
+function resolveVirtualModuleId<TModuleId extends string>(
+  id: TModuleId,
+): `\0${TModuleId}` {
+  return `\0${id}`;
 }
 
 export interface StarlightPluginsDocsComponentsContext {
-    githubOwner: string;
-    pluginName: string;
-    showcaseFilepath: string;
-    showcaseProps: {
-        entries: {
-            thumbnail: Promise<{
-                default: ImageMetadata;
-            }>;
-            href: string;
-            title: string;
-            description?: string;
-        }[];
-    };
+  githubOwner: string;
+  pluginName: string;
+  showcaseFilepath: string;
+  showcaseProps: {
+    entries: {
+      thumbnail: Promise<{
+        default: ImageMetadata;
+      }>;
+      href: string;
+      title: string;
+      description?: string;
+    }[];
+  };
 }
 
 type VitePlugin = NonNullable<ViteUserConfig["plugins"]>[number];
--- packages/starlight-plugins-docs-components/virtual.d.ts
+++ packages/starlight-plugins-docs-components/virtual.d.ts
@@ -1,4 +1,4 @@
 declare module "virtual:starlight-plugins-docs-components-context" {
-    const StarlightPluginsDocsComponentsContext: import("./libs/vite").StarlightPluginsDocsComponentsContext;
-    export default StarlightPluginsDocsComponentsContext;
+  const StarlightPluginsDocsComponentsContext: import("./libs/vite").StarlightPluginsDocsComponentsContext;
+  export default StarlightPluginsDocsComponentsContext;
 }
