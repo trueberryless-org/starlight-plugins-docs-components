22d9c74f8ac7357848328fce4d93132284cc52fc
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2025-11-22 17:55:25 +0100

    Site-wide banner

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,7 +1,16 @@
-import { docsLoader } from "@astrojs/starlight/loaders";
-import { docsSchema } from "@astrojs/starlight/schema";
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from 'astro:content'
+import { docsLoader } from '@astrojs/starlight/loaders'
+import { docsSchema } from '@astrojs/starlight/schema'
 
 export const collections = {
-  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-};
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema({
+      extend: z.object({
+        banner: z.object({ content: z.string() }).default({
+          content: 'This package is depricated. Please migrate your project following [this blog](https://blog.trueberryless.org/blog/starlight-customize-toc-overview-title/)',
+        }),
+      }),
+    }),
+  }),
+}
