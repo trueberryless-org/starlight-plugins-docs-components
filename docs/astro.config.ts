8051baf9b145ff2d351ecd2431498519ad2ff26b
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2026-03-13 18:49:04 +0100

    refactor: remove logos, use emojies (#60)

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -7,11 +7,6 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Starlight Plugins Docs Components",
-      logo: {
-        light: "./src/assets/logo-white.png",
-        dark: "./src/assets/logo-dark.png",
-        replacesTitle: true,
-      },
       editLink: {
         baseUrl:
           "https://github.com/trueberryless-org/starlight-plugins-docs-components/edit/main/docs/",
